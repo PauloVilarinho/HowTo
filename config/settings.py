98307b6a5d7e400a5b2f26ea95ce182a6e41ad11
Natanael <nataso.s11@gmail.com>
2019-12-05 22:47:18 -0300

    feat: throttling

--- config/settings.py
+++ config/settings.py
@@ -120,6 +120,15 @@ REST_FRAMEWORK = {
         'rest_framework.filters.SearchFilter',
         'rest_framework.filters.OrderingFilter',
     ),
+
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '15/hour',
+        'user': '45/hour'
+    }
 }
 
 SIMPLE_JWT = {
